dd6c9df98e8ec762afa3ddf5db817f23b6c86fef
penzata <penzata86@yahoo.com>
2023-02-14 09:24:34 +0200

    packeging

--- app/components/About.js
+++ app/components/About.js
@@ -4,9 +4,15 @@ import Page from "./Page"
 function About() {
   return (
     <Page title="About Us">
-      <h2>About Us!!!!!!</h2>
-      <p className="lead text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis dolorum labore quisquam vel id dicta fuga! Ducimus, quo. Dolore commodi aliquid error veritatis consequuntur, excepturi cumque fuga eum incidunt doloremque?</p>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita voluptates quisquam possimus tenetur, dicta enim rerum quis, quaerat id nobis provident quo dolorum sapiente temporibus facere non repellendus consequatur cupiditate!</p>
+      <h2>About Us</h2>
+      <br></br>
+      <h4>SoftServe trainees:</h4>
+      <ul>
+        <p className="lead text-muted">Gerasim Simeonov </p>
+        <p className="lead text-muted">Martin Petrov </p>
+        <p className="lead text-muted">Oleg Kostrytsyn </p>
+        <p className="lead text-muted">Veselin Petkov </p>
+      </ul>
     </Page>
   )
 }
