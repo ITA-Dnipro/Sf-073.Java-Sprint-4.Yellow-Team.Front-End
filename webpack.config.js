574cec8caacb1a8ea2783ea0f0d78a54c5806228
Veselin Petkov <warnimal128@gmail.com>
2023-02-14 02:46:10 +0200

    webpack fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,35 @@
-import { resolve, join } from "path"
+const path = require("path")
 
-export const entry = "./app/Main.js"
-export const output = {
-  publicPath: "/",
-  path: resolve(__dirname, "app"),
-  filename: "bundled.js"
-}
-export const mode = "development"
-export const devtool = "source-map"
-export const devServer = {
-  port: 3000,
-  static: {
-    directory: join(__dirname, "app")
+module.exports = {
+  entry: "./app/Main.js",
+  output: {
+    publicPath: "/",
+    path: path.resolve(__dirname, "app"),
+    filename: "bundled.js"
   },
-  hot: true,
-  liveReload: false,
-  historyApiFallback: { index: "index.html" }
-}
-export const module = {
-  rules: [
-    {
-      test: /\.jsx?$/,
-      exclude: /(node_modules)/,
-      use: {
-        loader: "babel-loader",
-        options: {
-          presets: ["@babel/preset-react", ["@babel/preset-env", { targets: { node: "12" } }]]
+  mode: "development",
+  devtool: "source-map",
+  devServer: {
+    port: 3000,
+    static: {
+      directory: path.join(__dirname, "app")
+    },
+    hot: true,
+    liveReload: false,
+    historyApiFallback: { index: "index.html" }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", ["@babel/preset-env", { targets: { node: "12" } }]]
+          }
         }
       }
-    }
-  ]
+    ]
+  }
 }
