2d537bb2bdb4b236fafe739dabaf28d181239b08
Veselin Petkov <warnimal128@gmail.com>
2023-02-14 20:04:32 +0200

    fixed condition

--- app/utils/PrivateRoutes.js
+++ app/utils/PrivateRoutes.js
@@ -3,8 +3,6 @@ import { Navigate, Outlet } from "react-router-dom";
 
 const PrivateRoutes = (role) => {
   let localStorageRole = localStorage.getItem("role");
-
-  console.log(localStorageRole, role.role, role.role2);
-  return localStorageRole == role.role || localStorageRole == role.role2 ? <Outlet /> : <Navigate to="/unauthorized" />;
+  return localStorageRole === role.role || localStorageRole === role.role2 ? <Outlet /> : <Navigate to="/unauthorized" />;
 };
 export default PrivateRoutes;
