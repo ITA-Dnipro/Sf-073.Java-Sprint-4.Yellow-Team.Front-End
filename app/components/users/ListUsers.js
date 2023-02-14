4a1fdbb627d55650259e014354b24c098b03b6a9
Martin0071 <marto007.1998@gmail.com>
2023-02-14 12:53:57 -0800

    made lists better

--- app/components/users/ListUsers.js
+++ app/components/users/ListUsers.js
@@ -29,22 +29,55 @@ export default class ListUsers extends Component {
 
   render() {
     if (this.state.listUsers[0] != null) {
+      // return (
+      //   <div>
+      //     <div className="container">
+      //       {this.state.listUsers &&
+      //         this.state.listUsers.map((user, ind) => (
+      //           <div key={user.id}>
+      //             {user.id}
+      //             <br />
+      //             {user.name}
+      //             <br /> {user.username} <br /> {user.role} <br />
+      //             <br />
+      //           </div>
+      //         ))}
+      //     </div>
+      //   </div>
+      // )
       return (
-        <div>
-          <div className="container">
-            {this.state.listUsers &&
-              this.state.listUsers.map((user, ind) => (
-                <div key={user.id}>
-                  {user.id}
-                  <br />
-                  {user.name}
-                  <br /> {user.username} <br /> {user.role} <br />
-                  <br />
-                </div>
-              ))}
+        <div className="maincontainer">
+          <div className="py-4">
+            <table className="table border shadow">
+              <thead>
+                <tr>
+                <th scope="col">ID</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Username</th>
+                  <th scope="col">Role</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.listUsers &&
+               this.state.listUsers.map((user) => (
+                  <tr>
+                    <th scope="row">
+                      {user.id}
+                    </th>
+                    <td>{user.name}</td>
+                    <td>
+                    {user.username}
+                    </td>
+                    <td>
+                    {user.role}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
         </div>
-      )
+      );
     }
   }
 }
