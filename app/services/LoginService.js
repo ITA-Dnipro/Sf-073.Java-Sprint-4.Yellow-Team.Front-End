e26e291731cf7cc6b8c5ca361f0b07a2a33f6e68
Oleg Kostrytsyn <62348736+okostritsyn@users.noreply.github.com>
2023-02-15 15:09:44 +0200

    change login to customAxios and add alert

--- app/services/LoginService.js
+++ app/services/LoginService.js
@@ -1,21 +1,42 @@
 import axios from "axios"
 import { Buffer } from "buffer"
 
-const LOGIN_REST_API_URL = "http://localhost:28852/api/auth/login"
+const LOGIN_REST_API_URL = "http://localhost:28852/api/auth"
 class LoginService {
-  async login(username, password) {
+  login(username, password) {
     const base64encodedData = Buffer.from(`${username}:${password}`).toString("base64")
-
-    const response = await axios.post(LOGIN_REST_API_URL, username, {
+    console.log(base64encodedData)
+    const customAxios = axios.create({
+      baseURL: LOGIN_REST_API_URL,
       headers: {
         "Content-Type": "application/json",
         Authorization: `Basic ${base64encodedData}`
       }
     })
-    localStorage.setItem("Authorization", `Basic ${base64encodedData}`)
-    localStorage.setItem("username", response.data.username)
-    localStorage.setItem("role", response.data.role)
-    window.location.href = "/logged"
+    customAxios.interceptors.response.use(
+      response => {
+        window.location.href = "/logged"
+        localStorage.setItem("Authorization", `Basic ${base64encodedData}`)
+        localStorage.setItem("username", response.data.username)
+        localStorage.setItem("role", response.data.role)
+      },
+      error => {
+        alert(error)
+        window.location.href = "/"
+      }
+    )
+    customAxios.post("login", username)
   }
+  //   const response = await axios.post(LOGIN_REST_API_URL, username, {
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Authorization: `Basic ${base64encodedData}`
+  //     }
+  //   })
+  //   localStorage.setItem("Authorization", `Basic ${base64encodedData}`)
+  //   localStorage.setItem("username", response.data.username)
+  //   localStorage.setItem("role", response.data.role)
+  //   window.location.href = "/logged"
+  // }
 }
 export default new LoginService()
