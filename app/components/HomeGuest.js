f8a5314e21f0d41992c2878dea5ae4ceb6d5ce38
Oleg Kostrytsyn <62348736+okostritsyn@users.noreply.github.com>
2023-02-15 11:19:48 +0200

    Change login page

--- app/components/HomeGuest.js
+++ app/components/HomeGuest.js
@@ -15,7 +15,7 @@ function HomeGuest() {
     try {
       RegisterService.register(name, username, password)
       console.log("User was successfully created.")
-      navigate("/")
+      navigate("/login")
     } catch (e) {
       alert("Error while creating user!")
       console.log("There was an error.")
--- app/components/SimpleLoginComponent.js
+++ app/components/SimpleLoginComponent.js
@@ -1,6 +1,8 @@
 import React, { Component, useState, useContext } from "react"
 
 import LoginService from "../services/LoginService"
+import Page from "./Page"
+
 var username
 var password
 const handleLogin = () => {
@@ -9,16 +11,18 @@ const handleLogin = () => {
 class SimpleLoginComponent extends Component {
   render() {
     return (
-      <Page title="Login page!" wide={false}>
+      <Page title="Login page" wide={false}>
         <div>
           <label>Username</label>
           <input onChange={e => (username = e.target.value)} type="text" className="form-control" id="username" aria-describedby="emailHelp" placeholder="Title" />
           <label>Password</label>
           <input onChange={e => (password = e.target.value)} type="password" className="form-control" id="password" aria-describedby="emailHelp" placeholder="Password" />
 
-          <button type="submit" className="btn btn-primary" onClick={handleLogin}>
-            Login
-          </button>
+          <p>
+            <button type="submit" className="py-3 mt-4 btn btn-lg btn-success btn-block" onClick={handleLogin}>
+              Login
+            </button>
+          </p>
         </div>
       </Page>
     )
